e18353d4e3ed08d3a543cf5f3f738fe0408d0551
chehaoyi <haoyiche@gmail.com>
2026-08-24 23:12:54 +0800

    Setup ai_base environment

---
+++ 02_Projects/environment_test/src/main.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+print("AI Lab Environment OK")
+print("Numpy version:", np.__version__)
+print("Pandas version:", pd.__version__)
+print("Matplotlib version:", plt.matplotlib.__version__)
